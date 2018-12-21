2834ca148162f1e53600f4f35ae315057978fa37
Pollard, Clarence <CLARENCE_POLLARD@homedepot.com>
2018-12-20 21:54:59 -0500

    more lifecycles

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -21,7 +21,9 @@ class Persons extends Component {
 
     shouldComponentUpdate(nextProps, nextState) {
       console.log('[UPDATE Persons.js] Inside shouldComponentUpdate', nextProps, nextState);
-      return nextProps.persons !== this.props.persons;
+      return nextProps.persons !== this.props.persons || 
+      nextProps.changed !== this.props.changed ||
+      nextProps.clicked !== this.props.clicked;
     }
 
     componentWillUpdate(nextProps, nextState) {
@@ -29,7 +31,7 @@ class Persons extends Component {
     }
 
     componentDidUpdate(nextProps, nextState) {
-      console.log('[UPDATE Persons.js] Inside componentDidUpdate', nextProps, nextState);      
+      console.log('[UPDATE Persons.js] Inside componentDidUpdate', nextProps, nextState);            
     }
         
     render() {
--- src/containers/App.js
+++ src/containers/App.js
@@ -27,7 +27,8 @@ class App extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
-    return true;
+    return nextState.persons !== this.state.persons || 
+          nextState.showPersons !== this.state.showPersons;
   }
 
   componentWillUpdate(nextProps, nextState) {
@@ -175,6 +176,7 @@ class App extends Component {
     return (
       // <StyleRoot>
       <div className="App">
+        <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
         <Cockpit
           appTitle={this.props.title}
           persons={this.state.persons}
