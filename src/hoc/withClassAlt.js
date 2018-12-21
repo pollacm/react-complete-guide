a75eb0b4001577329d4b8fbf2f7cf3f2c9080ada
Pollard, Clarence <CLARENCE_POLLARD@homedepot.com>
2018-12-21 10:01:27 -0500

    passing unknown props

--- src/hoc/withClassAlt.js
+++ src/hoc/withClassAlt.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+// const withClassAlt = (WrappedComponent, className) => {
+//     return (props) => (
+//         <div className={className}>
+//             <WrappedComponent {...props} />
+//         </div>
+//     )
+// }
 
 const withClassAlt = (WrappedComponent, className) => {
-    return (props) => (
-        <div className={className}>
-            <WrappedComponent />
-        </div>
-    )
+    return class extends Component {
+        render() {
+            return <div className={className}>
+            <WrappedComponent {...this.props} />
+            </div>
+        }
+    }
 }
 
 export default withClassAlt;
